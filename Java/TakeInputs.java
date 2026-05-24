66cb6d8d6a81d74f658faf5b90b674866d9b4271
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-24 10:21:06 +0530

    add TakeInputs.java for user input handling

---
+++ Java/TakeInputs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TakeInputS {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+       System.out.print("Enter your roll no. : ");
+
+       int rollno = input.nextInt();
+
+       System.out.println("Your roll number is: " + rollno);
+
+
+       String name = input.next();
+
+       System.out.println(name);
+
+       float marks = input.nextFloat();
+
+       System.out.println(marks);
+    }
+}
