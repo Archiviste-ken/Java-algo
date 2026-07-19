f89a7b96f02c70410c5aef3a96b41f9b5dbd671a
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-19 14:27:33 +0530

    Create boss_challenge.java for basic price calculation

---
+++ Module_1/boss_challenge.java
@@ -0,0 +1,12 @@
+class boss_challenge{
+    public static void main(String[] args) {
+        int price = 499;
+        int quantity = 3;
+        int total_price = price * quantity;
+
+        System.out.println("Price : " + price);
+        System.out.println("Quantity : " + quantity);
+        System.out.println("total_price : " + total_price);
+
+    }
+}
