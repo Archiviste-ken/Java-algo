361a3758d53f5f844b0773099eaa336663f73bc0
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-19 19:13:55 +0530

    Add input.java and Main.class for user input handling

---
+++ Module_2/input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+ class input {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        System.out.println(age);
+    }
+}
