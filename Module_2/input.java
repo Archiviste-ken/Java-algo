64a3a09bcc068ead3c853d2e232ea4a328ec60b4
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-20 11:16:43 +0530

    Refactor input.java to uncomment and organize code structure

--- Module_2/input.java
+++ Module_2/input.java
@@ -1,10 +1,22 @@
+// import java.util.Scanner;
+
+//  class input {
+
+//     public static void main(String[] args) {
+
+
+//         Scanner sc = new Scanner(System.in);
+
+//         int age = sc.nextInt();
+
+//         System.out.println(age);
+//     }
+// }
+
 import java.util.Scanner;
 
- class input {
-
-    public static void main(String[] args) {
-
-
+class input{
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
 
         int age = sc.nextInt();
