87f0c1b79ab7a4232c89889facb3e8682ee4e3ec
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-22 13:49:54 +0530

    Add Return class with add method and main for demonstration

---
+++ Module_3/return.java
@@ -0,0 +1,16 @@
+
+class Return {
+
+    public static int add(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+        int result = add(3, 5);
+
+        System.err.println(result);
+    }
+
+}
