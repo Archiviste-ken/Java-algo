337ff36857a9a76c8663996cf968470debf9c7c9
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-22 15:58:16 +0530

    Add enhancedFor class with main method demonstrating enhanced for loop

---
+++ Module_3/enhancedFor.java
@@ -0,0 +1,16 @@
+
+import java.lang.ref.Cleaner;
+
+public class enhancedFor {
+
+    public static void main(String[] args) {
+        // Enhanced For Loop
+        for (int num : arr) {
+
+            System.out.println(num);
+
+        }
+
+        // Cleaner.But no index.
+    }
+}
