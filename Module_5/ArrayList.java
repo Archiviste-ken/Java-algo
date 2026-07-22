585d20ab76bfb2d443676dc98fb502f3f80647fb
Shreyesh <shreyeshpachpor06@gmail.com>
2026-07-22 23:22:32 +0530

    Add ArrayList.java with introductory comments on dynamic arrays

---
+++ Module_5/ArrayList.java
@@ -0,0 +1,21 @@
+// ⭐ ArrayList (Most Used)
+
+// Think of it as a dynamic array.
+
+// Instead of
+
+// int[] arr = new int[5];
+
+// Use
+
+// ArrayList<Integer> list = new ArrayList<>();
+
+// Notice
+
+// Integer
+
+// NOT
+
+// int
+
+// We'll learn why in a minute.
